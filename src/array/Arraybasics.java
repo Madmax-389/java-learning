0d463f70b03d7d3e6f56cc33564d3e388108df95
Mohit Tiwari <mohit512.om2010@gmail.com>
2026-02-04 01:48:28 +0530

    Array basics and stack and heap understanding

---
+++ src/array/Arraybasics.java
@@ -0,0 +1,70 @@
+package array;
+
+import java.util.Scanner;
+
+public class Arraybasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Q1 - Print all the elements 
+		/*Scanner sc = new Scanner(System.in);
+		
+int[] arr = new int[4];
+System.out.println("Enter the elements");
+for (int i=0;i<arr.length;i++) {
+	arr[i]=sc.nextInt();
+}
+	System.out.println("All the elements are ");
+	for (int i=0;i<arr.length;i++) {
+		System.out.print(arr[i]+" ");
+	}*/
+		//Q2 - Find sum of elements 
+		/*Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[4];
+		System.out.println("Enter the elements");
+		for (int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+	int sum =0;
+	for (int i=0;i<arr.length;i++) {
+		sum+=arr[i];
+	}
+	System.out.println(sum);*/
+	
+		//Q3- Find max value 
+/*Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[4];
+		System.out.println("Enter the elements");
+		for (int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int max =0;
+		for (int i=0;i<arr.length;i++) {
+			if (arr[i]>max) {
+				max =arr[i];
+			}
+		}
+		
+		System.out.println("Max value is "+max);*/
+Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[4];
+		System.out.println("Enter the elements");
+		for (int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+		int count =0;
+		for (int i=0;i<arr.length;i++) {
+			if (!((arr[i]%2)==0)) {
+				count++;
+			}
+		}
+		
+		System.out.println("THe number of odd elements are "+ count);
+		
+	}
+
+}
--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -10,7 +10,38 @@ public class HelloWorld {
 		String a = "Hello";
 		String b = "Hello";
 		System.out.println(a==b);
+		//a is having a stack lets say 1001 and we store the string "Hello" in the string pool, now when we declare b as "Hello" again java recognises the value and to avoid duplicasy it assigns the same stack 1001 hence it is true 
+		static void change(int[] a) {
+		    a[0] = 5;
+		    a = new int[]{7,7,7};
+		}
+
+		int[] nums = {1,2,3};
+		change(nums);
+		System.out.println(nums[0]);
 		
+		/*This prints 5.
+
+Before the function call, nums stores address 5001,
+and heap at 5001 contains {1,2,3}.
+
+When change(nums) is called, Java passes a COPY of the reference (5001),
+so both nums and a point to the same heap object.
+
+When we do a[0] = 5, we modify the object stored at heap address 5001,
+so the heap becomes {5,2,3}. This change is visible through nums.
+
+Next, a = new int[]{7,7,7} creates a NEW array at a different address (say 6001),
+and only the local variable 'a' now points to 6001.
+
+nums still points to the original 5001.
+
+After the method finishes, the stack frame for 'a' is destroyed,
+so nums still refers to {5,2,3}.
+
+Therefore, nums[0] prints 5.
+		 */
+
 		    }
 		}
 	
