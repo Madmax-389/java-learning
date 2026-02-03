e59b6957f20e2b6ba888b343c5695a0232324f81
Mohit Tiwari <mohit512.om2010@gmail.com>
2026-02-04 00:20:46 +0530

    Added memory vs value logic

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -5,7 +5,12 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		        System.out.println("Git + Eclipse connected!");
+		        //System.out.println("Git + Eclipse connected!");
+		
+		String a = "Hello";
+		String b = "Hello";
+		System.out.println(a==b);
+		
 		    }
 		}
 	
